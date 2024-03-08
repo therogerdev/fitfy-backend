2daf550e5a36dc1b43c03ef25806fd69be5d37b3
Rogerio Menezes <rogeriomenezes.nz@gmail.com>
2024-03-08 16:12:28 +1300

    chore: add middlewares

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import express, {Express, Request, Response} from 'express'
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from './config/morgan.js';
+import compression from 'compression';
+import 'dotenv/config'
+import { limiter } from './middleware/rateLimiter.js';
+import athleteRouter from './routes/athleteRouter.js'
+import config from './config/config.js';
+
+
+
+const app:Express = express()
+
+const PORT = process.env.PORT
+
+
+if (config.env !== 'test') {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
+
+// middleware
+app.use(express.json())
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(limiter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({message: "Hello World!!" });
+})
+
+app.use("/api/athletes", athleteRouter )
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`
+🚀 Server ready at: http://localhost:${PORT}`),
+)
