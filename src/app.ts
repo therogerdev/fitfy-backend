ac2bb912265d74f4c44542a41e209e22c96ba665
Rogerio Menezes <rogeriomenezes.nz@gmail.com>
2024-03-08 16:15:08 +1300

    chore: add lint

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,16 @@
-import express, {Express, Request, Response} from 'express'
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from './config/morgan.js';
 import compression from 'compression';
-import 'dotenv/config'
+import 'dotenv/config';
 import { limiter } from './middleware/rateLimiter.js';
-import athleteRouter from './routes/athleteRouter.js'
+import athleteRouter from './routes/athleteRouter.js';
 import config from './config/config.js';
 
+const app: Express = express();
 
-
-const app:Express = express()
-
-const PORT = process.env.PORT
-
+const PORT = process.env.PORT;
 
 if (config.env !== 'test') {
   app.use(morgan.successHandler);
@@ -21,22 +18,19 @@ if (config.env !== 'test') {
 }
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(limiter);
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({message: "Hello World!!" });
-})
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello World!!' });
+});
 
-app.use("/api/athletes", athleteRouter )
-
-
-
+app.use('/api/athletes', athleteRouter);
 
 app.listen(PORT, () =>
   console.log(`
-🚀 Server ready at: http://localhost:${PORT}`),
-)
+🚀 Server ready at: http://localhost:${PORT}`)
+);
