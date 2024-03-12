18817a4e5b3082f3a385303b66ca4fae25dd2c6a
Rogerio <91988248+rogerio-js@users.noreply.github.com>
2024-03-12 16:26:08 +1300

    chore: add type from prisma

--- src/services/movementService.ts
+++ src/services/movementService.ts
@@ -4,15 +4,15 @@ import prisma from "../prismaClient.js";
 export const getAllMovements = async (category: Prisma.MovementCreateInput["category"]) => {
   return await prisma.movement.findMany({
     where: {
-      category: category
+      category
     }
   });
 };
 
-export const getMovementById = async (id: Prisma.MovementCreateInput["id"]) => {
+export const getMovementById = async (args: Prisma.MovementFindUniqueArgs) => {
   return await prisma.movement.findUnique({
     where: {
-      id: id
+      id: args.where.id
     }
   });
 };
