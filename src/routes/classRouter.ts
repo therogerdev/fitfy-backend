bec3a096b90a08ee854c4630a788208cf43ee234
Rogerio <91988248+rogerio-js@users.noreply.github.com>
2024-10-25 17:29:14 +0700

    cancel check-in

--- src/routes/classRouter.ts
+++ src/routes/classRouter.ts
@@ -4,12 +4,16 @@ import { listClass } from "../endpoints/Class/controller/listClass.controller.js
 import { enrollClass } from "../endpoints/Class/controller/enrollClass.controller.js";
 import { cancelEnrollment } from "../endpoints/Class/controller/cancelEnrollment.controller.js";
 import { listEnrollment } from "../endpoints/Class/controller/listEnrollment.controller.js";
+import { classAttendance } from "../endpoints/Class/controller/classAttendance.controller.js";
+import { cancelClassAttendance } from "../endpoints/Class/controller/cancelClassAttendance.controller.js";
 
 const classRouter = Router();
 classRouter.get("/list", listClass);
 classRouter.post("/create", createClass);
 
 classRouter.get("/:classId/enrollment", listEnrollment);
+classRouter.post("/:classId/attendance", classAttendance);
+classRouter.patch("/:enrollmentId/attendance", cancelClassAttendance);
 
 // for athletes to enroll in the class
 classRouter.post("/:classId/enroll", enrollClass);
